500ca56e9e6a3968797db9d73216aecf00a7b178
Marc et Nancy <famille@Moon-2.local>
2018-08-08 10:40:26 -0400

    Initial emacs config commit

---
+++ init.el
@@ -0,0 +1,76 @@
+;;; This fixed garbage collection, makes emacs start up faster ;;;;;;;
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; This is all kinds of necessary
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;;; remove SC if you are not using sunrise commander and org if you like outdated packages
+(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Bootstrapping use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+;;; Anything below is personal preference.
+;;; I recommend changing these values with the "customize" menu
+;;; You can change the font to suit your liking, it won't break anything.
+;;; The one currently set up is called Terminus.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (js2-mode auctex yaml-mode markdown-mode json-mode csharp-mode batch-mode p4 which-key diminish org-bullets htmlize magit company-shell company-lua slime-company slime company-jedi company-irony company-c-headers flycheck-clang-analyzer flycheck yasnippet-snippets yasnippet expand-region rainbow-delimiters rainbow-mode beacon hemisu-dark use-package)))
+ '(safe-local-variable-values
+   (quote
+    ((eval progn
+	   (require
+	    (quote projectile))
+	   (setq projectile-default-run-command "crawler")
+	   (setq company-clang-arguments
+		 (delete-dups
+		  (append company-clang-arguments
+			  (list
+			   (concat "-I"
+				   (projectile-project-root)
+				   "inc")))))
+	   (setq flycheck-clang-include-path
+		 (delete-dups
+		  (append flycheck-clang-include-path
+			  (list
+			   (concat
+			    (projectile-project-root)
+			    "inc"))))))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
